81bcd330a4650add7c4f31566e73f1476a64a513
Luca L. Bow <luca@doubleelbow.com>
2018-08-19 19:18:40 +0200

    Change maven groupId and artifactId

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.doubleelbow/capital.file "0.1.0-SNAPSHOT"
+(defproject com.doubleelbow.capital/capital-file "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/doubleelbow/capital-file"
   :license {:name "Eclipse Public License"
